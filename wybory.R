8821d8de3a7bcf6b29fc5783c2f8bc44c7b62f8a
TomekMatuszek <tom.mateuszek@gmail.com>
2020-05-12 00:59:01 +0200

    Poprawiono komunikat ostrzeżenia

--- wybory.R
+++ wybory.R
@@ -8,8 +8,8 @@ wybory = function(kom1, kom2, kom3, kom4, kom5, okreg){
   if (is.numeric(okreg) == FALSE || round(okreg, 0) != okreg || okreg > 41 || okreg < 1){
     stop("Wybierz numer okręgu z przedziału liczb całkowitych (1,41)!")
   }
-  if (sum(kom1, kom2, kom3, kom4, kom5) < 95){
-    warning("Suma poparcia wszystkich komitetów nie mieści się w przedziale (95,100). Lepiej, aby suma głosów nieważnych oraz tych na mniejsze komitety nie przekraczała 5%.")
+  if (sum(kom1, kom2, kom3, kom4, kom5) < 90){
+    warning(paste("Suma poparcia wszystkich komitetów wynosi", sum(kom1, kom2, kom3, kom4, kom5), "%. \n To oznacza, że", 100 - sum(kom1, kom2, kom3, kom4, kom5), "% głosów została oddana nieważnych lub na komitety, które nie przekroczyły progu wyborczego."))
   }
   system_dhonta <<- as.matrix(dhont(kom1, kom2, kom3, kom4, kom5, okreg))
   system_sainte_lague <<- as.matrix(sainte_lague(kom1, kom2, kom3, kom4, kom5, okreg))
@@ -17,4 +17,4 @@ wybory = function(kom1, kom2, kom3, kom4, kom5, okreg){
   wyniki <<- cbind(system_dhonta, system_sainte_lague, system_hare_niemeyer)
   wyniki
 }
-wybory(45, 15, 18, 12, 10, 39)
+wybory(53.29, 18.57, 11.31, 8.17, 2.89, 39)
