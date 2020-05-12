a34abbf4a785868475fdd82ab2bc6bfa30c04721
TomekMatuszek <tom.mateuszek@gmail.com>
2020-05-12 21:20:15 +0200

    Zmieniono nazwę kolumny

--- macierz_okregow.R
+++ macierz_okregow.R
@@ -1,3 +1,3 @@
 okregi = read.csv("okregi_sejm.csv", sep = ";")
 okregi = matrix(c(okregi[ ,7], okregi[ ,3]), ncol = 2, nrow = 41)
-colnames(okregi) = c("Liczba ludności", "Liczba mandatów")
+colnames(okregi) = c("Liczba wyborców", "Liczba mandatów")
