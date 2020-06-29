805a28b5af85648ddf44597d5df8720fcc5b5042
TomekMatuszek <tom.mateuszek@gmail.com>
2020-06-30 00:18:55 +0200

    Rozbudowanie funkcji wybory_pl o inne metody obliczania liczby mandatów

--- wybory_pl.R
+++ wybory_pl.R
@@ -4,9 +4,22 @@ wybory_pl = function(kom1, kom2, kom3, kom4, kom5, frekwencja = 100){
     m2 = dhont(kom1, kom2, kom3, kom4, kom5, i, frekwencja = 100)
     m = cbind(m, m2)
   }
-  m3 = matrix(rowSums(m, na.rm = TRUE), ncol = 1, nrow = 5)
+  dh = rowSums(m, na.rm = TRUE)
+  m = matrix( , ncol = 1, nrow = 5)
+  for (i in 1:nrow(okregi)) {
+    m2 = sainte_lague(kom1, kom2, kom3, kom4, kom5, i, frekwencja = 100)
+    m = cbind(m, m2)
+  }
+  sl = rowSums(m, na.rm = TRUE)
+  m = matrix( , ncol = 1, nrow = 5)
+  for (i in 1:nrow(okregi)) {
+    m2 = hare_niemeyer(kom1, kom2, kom3, kom4, kom5, i, frekwencja = 100)
+    m = cbind(m, m2)
+  }
+  hn = rowSums(m, na.rm = TRUE)
+  m3 = matrix(c(dh, sl, hn), ncol = 3, nrow = 5)
   rownames(m3) = c("Komitet I", "Komitet II", "Komitet III", "Komitet IV", "Komitet V")
-  colnames(m3) = "mandaty"
+  colnames(m3) = c("D'Hont", "Sainte-Lague", "Hare-Niemeyer")
   m3
 }
-wybory_pl(35, 29, 25, 6, 5)
+wybory_pl(42, 31, 12, 9, 6)
