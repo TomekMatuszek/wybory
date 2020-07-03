403ebd38b135a22fab4ddfb2c9e71ac673c4668f
TomekMatuszek <tom.mateuszek@gmail.com>
2020-07-03 11:32:00 +0200

    Wprowadzono elementy programowania obiektowego. Do funkcji wybory_rok można wprowadzić jedynie macierz stworzoną przy pomocy konstruktora w pliku macierz_okregow

--- macierz_okregow.R
+++ macierz_okregow.R
@@ -2,8 +2,12 @@ okregi = read.csv("okregi_sejm.csv", sep = ";")
 okregi = matrix(c(okregi[ ,7], okregi[ ,3]), ncol = 2, nrow = 41)
 colnames(okregi) = c("Liczba wyborców", "Liczba mandatów")
 
-okregi_wyniki = read.csv("sejm_wyniki.csv", sep = ";")
-okregi_wyniki = matrix(c(okregi_wyniki[ ,9], okregi_wyniki[ ,11], okregi_wyniki[ ,12], 
-                         okregi_wyniki[ ,14], okregi_wyniki[ ,16], okregi_wyniki[ ,18]),
-                       nrow = 41, ncol = 6)
-colnames(okregi_wyniki) = c("KO", "Konf", "PSL", "PiS", "Lewica", "MN")
+konstruktor_wynikow = function(nazwa, kol1, kol2, kol3, kol4, kol5){
+  okregi_wyniki = read.csv(nazwa, sep = ";")
+  okregi_wyniki = matrix(c(okregi_wyniki[ ,kol1], okregi_wyniki[ ,kol2], okregi_wyniki[ ,kol3], 
+                           okregi_wyniki[ ,kol4], okregi_wyniki[ ,kol5]),
+                         nrow = 41, ncol = 5)
+  colnames(okregi_wyniki) = c("Kom1", "Kom2", "Kom3", "Kom4", "Kom5")
+  okregi_wyniki = structure(okregi_wyniki, class = "macierz_wynikow")
+}
+konstruktor_wynikow("sejm_wyniki.csv", 9, 11, 12, 14, 16)
--- wybory_2019.R
+++ wybory_2019.R
@@ -1,31 +1,35 @@
-wybory_2019 = function(){
+wybory_rok = function(wyniki){
+  UseMethod("wybory_2019")
+}
+
+wybory_rok.macierz_wynikow = function(wyniki){
   dh_mx = matrix( ,nrow = 5, ncol = 1)
-  for (i in 1:nrow(okregi_wyniki)) {
-    dh = dhont(okregi_wyniki[i, 1], okregi_wyniki[i, 2], okregi_wyniki[i, 3], 
-               okregi_wyniki[i, 4] - 0.01, okregi_wyniki[i, 5], i)
+  for (i in 1:nrow(wyniki)) {
+    dh = dhont(wyniki[i, 1], wyniki[i, 2], wyniki[i, 3], 
+               wyniki[i, 4] - 0.01, wyniki[i, 5], i)
     dh_mx = cbind(dh_mx, dh)
   }
   dh_sum = rowSums(dh_mx, na.rm = TRUE)
   
   sl_mx = matrix( ,nrow = 5, ncol = 1)
-  for (i in 1:nrow(okregi_wyniki)) {
-    sl = sainte_lague(okregi_wyniki[i, 1], okregi_wyniki[i, 2], okregi_wyniki[i, 3], 
-               okregi_wyniki[i, 4] - 0.01, okregi_wyniki[i, 5], i)
+  for (i in 1:nrow(wyniki)) {
+    sl = sainte_lague(wyniki[i, 1], wyniki[i, 2], wyniki[i, 3], 
+               wyniki[i, 4] - 0.01, wyniki[i, 5], i)
     sl_mx = cbind(sl_mx, sl)
   }
   sl_sum = rowSums(sl_mx, na.rm = TRUE)
   
   hn_mx = matrix( ,nrow = 5, ncol = 1)
-  for (i in 1:nrow(okregi_wyniki)) {
-    hn = hare_niemeyer(okregi_wyniki[i, 1], okregi_wyniki[i, 2], okregi_wyniki[i, 3], 
-                      okregi_wyniki[i, 4] - 0.01, okregi_wyniki[i, 5], i)
+  for (i in 1:nrow(wyniki)) {
+    hn = hare_niemeyer(wyniki[i, 1], wyniki[i, 2], wyniki[i, 3], 
+                      wyniki[i, 4] - 0.01, wyniki[i, 5], i)
     hn_mx = cbind(hn_mx, hn)
   }
   hn_sum = rowSums(hn_mx, na.rm = TRUE)
   
-  wyniki = matrix(c(dh_sum, sl_sum, hn_sum), ncol = 3, nrow = 5)
-  colnames(wyniki) = c("D'Hont", "Sainte-Lague", "Hare-Niemeyer")
-  rownames(wyniki) = c("KO", "Konf", "PSL", "PiS", "Lewica")
-  wyniki
+  wyniki_man = matrix(c(dh_sum, sl_sum, hn_sum), ncol = 3, nrow = 5)
+  colnames(wyniki_man) = c("D'Hont", "Sainte-Lague", "Hare-Niemeyer")
+  rownames(wyniki_man) = c("Komitet I", "Komitet II", "Komitet III", "Komitet IV", "Komitet V")
+  wyniki_man
 }
-wybory_2019()
+wybory_rok(okregi_wyniki)
