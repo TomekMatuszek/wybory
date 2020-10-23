0539234dcad2ef88e74f8f104b14cb134133a2ab
TomekMatuszek <tom.mateuszek@gmail.com>
2020-10-23 15:22:09 +0200

    Dodano opisy wykresu oraz komentarze

--- wybory_rok.R
+++ wybory_rok.R
@@ -1,3 +1,7 @@
+#Ponizsza funkcja oblicza podzial mandatów wg roznych metod na podstawie
+#Macierzy wyników utworzonej przy pomocy konstruktora
+#Wynikiem jest macierz przedstawiajaca liczbe zdobytych mandatów przez każdy komitet
+#Oraz wykres slupkowy z zaznaczonym realnym poparciem partii a także progiem 230 mandatów
 wybory_rok = function(wyniki){
   UseMethod("wybory_rok")
 }
@@ -32,8 +36,14 @@ wybory_rok.macierz_wynikow = function(wyniki){
   rownames(wyniki_man) = c("Komitet I", "Komitet II", "Komitet III", "Komitet IV", "Komitet V")
   
   barplot(wyniki_man, beside = TRUE, col = c("orange", "black", "darkgreen", "blue", "red"),
-          ylim = c(0,250))
+          ylim = c(0,250), ylab = "Liczba mandatów", xlab = "Metoda obliczania podzialu mandatów",
+          main = "Podzial mandatów w Sejmie")
   abline(h = 230, col = "red", lwd = 2)
+  abline(h = 460 * (mean(wyniki[ ,1]) / 100), col = "orange", lwd = 1.5)
+  abline(h = 460 * (mean(wyniki[ ,2]) / 100), col = "black", lwd = 1.5)
+  abline(h = 460 * (mean(wyniki[ ,3]) / 100), col = "darkgreen", lwd = 1.5)
+  abline(h = 460 * (mean(wyniki[ ,4]) / 100), col = "blue", lwd = 1.5)
+  abline(h = 460 * (mean(wyniki[ ,5]) / 100), col = "red", lwd = 1.5)
   text(18, 240, "230", col = "red")
   
   wyniki_man
