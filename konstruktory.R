bdb5e3d867c18890b1964223cab2754c9b8544f8
TomekMatuszek <tom.mateuszek@gmail.com>
2020-10-23 14:23:02 +0200

    Zmiana nazwy pliku oraz dodanie komentarzy

--- konstruktory.R
+++ konstruktory.R
@@ -1,7 +1,12 @@
+#Ponizsze linijki kodu tworza macierz okregow wyborczych
+#Zawiera ona liczbe wyborcow w danym okregu oraz przypadajacych na niego mandatow
 okregi = read.csv("okregi_sejm.csv", sep = ";")
 okregi = matrix(c(okregi[ ,7], okregi[ ,3]), ncol = 2, nrow = 41)
 colnames(okregi) = c("Liczba wyborców", "Liczba mandatów")
 
+#Konstruktor wynikow tworzy macierz wynikow o strukturze pozwalajacej uzyc funkcji przydzielajacych mandaty
+#Nalezy wpisac sciezke do pobranego przez nas pliku excel z wynikami wyborow ze strony sejm.gov.pl
+#Kolejne argumenty (max 5) oznaczaja numery kolumn w ktorych znajduja sie wyniki interesujacych nas komitetow
 konstruktor_wynikow = function(nazwa, kol1, kol2, kol3, kol4, kol5){
   okregi_wyniki <<- read.csv(nazwa, sep = ";")
   okregi_wyniki <<- matrix(c(okregi_wyniki[ ,kol1], okregi_wyniki[ ,kol2], okregi_wyniki[ ,kol3], 
