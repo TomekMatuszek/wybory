64b856b81cbd2649f4354248af4ce8a033c0fde9
TomekMatuszek <tom.mateuszek@gmail.com>
2020-11-04 18:24:00 +0100

    dodanie obsługi plików csv i xls w konstruktorze

--- konstruktory.R
+++ konstruktory.R
@@ -10,10 +10,13 @@ colnames(okregi) = c("Liczba wyborców", "Liczba mandatów")
 library(readxl)
 library(stringr)
 konstruktor_wynikow = function(nazwa, kol1, kol2, kol3, kol4, kol5){
-  okregi_wyniki = read_excel(nazwa, skip = 1, col_names = FALSE, .name_repair = "minimal")
-  okregi_wyniki = sapply(okregi_wyniki, as.numeric)
-  okregi_wyniki = as.data.frame(okregi_wyniki)
-  #okregi_wyniki = read.csv(nazwa, sep = ";")
+  if(str_extract(nazwa, pattern = "[\\.]+[a-z]{3}") == ".xls"){
+    okregi_wyniki = read_excel(nazwa, skip = 1, col_names = FALSE, .name_repair = "minimal")
+    okregi_wyniki = sapply(okregi_wyniki, as.numeric)
+    okregi_wyniki = as.data.frame(okregi_wyniki)
+  } else if(str_extract(nazwa, pattern = "[\\.]+[a-z]{3}") == ".csv"){
+    okregi_wyniki = read.csv(nazwa, sep = ";")
+  }
   okregi_wyniki = matrix(c(okregi_wyniki[ ,kol1], okregi_wyniki[ ,kol2], okregi_wyniki[ ,kol3], 
                            okregi_wyniki[ ,kol4], okregi_wyniki[ ,kol5]),
                          nrow = 41, ncol = 5)
@@ -23,13 +26,6 @@ konstruktor_wynikow = function(nazwa, kol1, kol2, kol3, kol4, kol5){
 }
 wybory_2019 = konstruktor_wynikow("sejm_wyniki_2019.xlsx", 9, 11, 12, 14, 16)
 wybory_2015 = konstruktor_wynikow("sejm_wyniki_2015.xls", 9, 10, 13, 15, 16)
-okregi_wyniki
+wybory_2019
+wybory_2015
 
-
-if(str_extract(nazwa, pattern = "[\\.]+[a-z]{3}") == "xls"){
-  okregi_wyniki = read_excel(nazwa, skip = 1, col_names = FALSE, .name_repair = "minimal")
-  okregi_wyniki = sapply(okregi_wyniki, as.numeric)
-  okregi_wyniki = as.data.frame(okregi_wyniki)
-} else if(str_extract(nazwa, pattern = "[\\.]+[a-z]{3}") == "csv"){
-  okregi_wyniki = read.csv(nazwa, sep = ";")
-}
