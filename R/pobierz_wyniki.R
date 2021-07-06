f799ef6e766ed6ace8cdeb8073d0bb76a56cd16a
TomekMatuszek <tom.mateuszek@gmail.com>
2021-07-06 17:58:47 +0200

    dodanie funkcji pobierającej wyniki

---
+++ R/pobierz_wyniki.R
@@ -0,0 +1,51 @@
+pobierz_wyniki = function(rok){
+  linki = readxl::read_excel("linki.xls")
+  if (rok == 2019 || rok == 2015){
+    download.file(linki$link_wyniki[linki$rok == rok],
+                  destfile = paste0("wyniki", rok, ".zip"))
+    download.file(linki$link_okregi[linki$rok == rok],
+                  destfile = paste0("okregi", rok, ".zip"))
+
+    unzip(paste0("wyniki", rok, ".zip"), exdir = "dane_wybory")
+    unzip(paste0("okregi", rok, ".zip"), exdir = "dane_wybory")
+  } else if (rok == 2011){
+    plik_html = xml2::read_html(linki$link_wyniki[linki$rok == 2011])
+    tabela_html = rvest::html_node(plik_html, "table.wikitable:nth-child(51)")
+    tabela_r = rvest::html_table(tabela_html, fill = TRUE, header = TRUE)
+    colnames(tabela_r) = tabela_r[1 ,]
+    wyniki2011 = tabela_r[2:42, ]
+    wyniki2011$Okręg = as.numeric(stringr::str_sub(wyniki2011$Okręg, start = 1, end = 2))
+    for (i in 3:13) {
+      wyniki2011[ , i] = as.numeric(stringr::str_replace_all(wyniki2011[ , i], "\\,", "\\."))
+    }
+    dir.create("dane_wybory")
+    writexl::write_xlsx(wyniki2011, "dane_wybory/wyniki2011.xlsx")
+
+    download.file(linki$link_okregi[linki$rok == 2011],
+                  destfile = paste0("okregi", 2011, ".zip"))
+    unzip(paste0("okregi", 2011, ".zip"), exdir = "dane_wybory")
+    okregi2011 = read.csv("dane_wybory/okregi.csv", sep = ";")
+    okregi2011 = okregi2011[1:41, 3:8]
+    colnames(okregi2011) = c("nr_okregu", "liczba_mandatow", "liczba_komitetow",
+                             "liczba_kandydatow", "liczba_wyborcow", "granice")
+    writexl::write_xlsx(okregi2011, "dane_wybory/okregi2011.xlsx")
+  } else if (rok == 2007){
+    plik_html = xml2::read_html(linki$link_wyniki[linki$rok == 2007])
+    tabela_html = rvest::html_node(plik_html, "table.wikitable:nth-child(100)")
+    tabela_r = rvest::html_table(tabela_html, fill = TRUE, header = TRUE)
+    colnames(tabela_r) = tabela_r[1 ,]
+    wyniki2007 = tabela_r[2:42, ]
+    wyniki2007$Okręg = as.numeric(stringr::str_sub(wyniki2007$Okręg, start = 1, end = 2))
+    for (i in 3:12) {
+      wyniki2007[ , i] = as.numeric(stringr::str_replace_all(wyniki2007[ , i], "\\,", "\\."))
+    }
+    dir.create("dane_wybory")
+    writexl::write_xlsx(wyniki2007, "dane_wybory/wyniki2007.xlsx")
+
+    plik_html = xml2::read_html(linki$link_okregi[linki$rok == 2007])
+    tabela_html = rvest::html_node(plik_html, "table.wikitable:nth-child(82)")
+    tabela_r = rvest::html_table(tabela_html, fill = TRUE, header = TRUE)
+    okregi2007 = tabela_r[, c(1, 3, 4, 6)]
+    writexl::write_xlsx(okregi2007, "dane_wybory/okregi2007.xlsx")
+  }
+}
