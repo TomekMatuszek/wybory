f81ad04c81d226720a3df62d67c84bad570157c8
TomekMatuszek <tom.mateuszek@gmail.com>
2021-10-28 12:23:01 +0200

    .

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 library(wybory)
 devtools::load_all()
 
-pobierz_wyniki(2007)
+pobierz_wyniki(2019)
 wybory_2019 = konstruktor_wynikow("dane_wybory/wyniki2019.xlsx", 9, 11, 12, 14, 16, 17, 18)
 wybory_2015 = konstruktor_wynikow("dane_wybory/wyniki2015.xls", 9, 10, 13, 15, 16, 24)
 wybory_2011 = konstruktor_wynikow("dane_wybory/wyniki2011.xlsx", 3, 4, 5, 6, 7, 8, 9, 11)
@@ -14,8 +14,10 @@ wybory_rok(wybory_2015)
 wybory_rok(wybory_2011)
 wybory_rok(wybory_2007)
 
-wykres_wyniki("dane_wybory/wyniki2019.xlsx", 9, 11, 12, 14, 16)
-wykres_wyniki("dane_wybory/wyniki2015.xls", 9, 10, 13, 15, 16)
+wykres_wyniki("dane_wybory/wyniki2019.xlsx", 9, 11, 12, 14, 16, 17, 18)
+wykres_wyniki("dane_wybory/wyniki2015.xls", 9, 10, 13, 15, 16, 24)
+wykres_wyniki("dane_wybory/wyniki2011.xlsx", 3, 4, 5, 6, 7, 8, 9, 11)
+wykres_wyniki("dane_wybory/wyniki2007.xlsx", 3, 4, 5, 6, 8, 10)
 
 ?konstruktor_okregow
 ?konstruktor_wynikow
