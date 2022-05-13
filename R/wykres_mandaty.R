a73936c0c6758d61d76a697175505f769417e37b
Tomek Matuszek <tommat1@st.amu.edu.pl>
2022-05-14 00:58:38 +0200

    zmiana miejsca obliczania poparcia, dodanie linijki zmieniającej kolory

--- R/wykres_mandaty.R
+++ R/wykres_mandaty.R
@@ -14,13 +14,15 @@ wykres_mandaty = function(wyniki){
   liczba_mandatow = sum(wyniki[, 2])
   wyniki_longer = tidyr::pivot_longer(wyniki, cols = 2:4,
                                       names_to = "metoda")
+  wyniki_longer$poparcie = wyniki_longer$poparcie / 100 * liczba_mandatow
 
   p = ggplot2::ggplot(wyniki_longer) +
     ggplot2::geom_bar(ggplot2::aes(x = metoda, y = value, fill = Komitet),
                       position = "dodge", stat = "identity") +
-    ggplot2::geom_bar(ggplot2::aes(x = metoda, y = poparcie / 100 * liczba_mandatow, fill = Komitet),
-                      colour = "black", alpha = 0, show.legend = FALSE,
+    ggplot2::geom_bar(ggplot2::aes(x = metoda, y = poparcie, fill = Komitet),
+                      colour = "black", alpha = 0.3, show.legend = FALSE,
                       position = "dodge", stat = "identity")
+  #+ ggplot2::scale_fill_manual(values = palette.colors(palette = "Set1"))
   if (liczba_mandatow == 460){
     p = p + ggplot2::geom_hline(yintercept = 230, size = 1.5, colour = "red")
   }
